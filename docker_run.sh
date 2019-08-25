81d576dcdcf3b08be38e87fadaf576fb57d373ca
Shubhi Gupta <shubhigupta@Shubhis-MacBook-Pro.local>
2019-08-26 00:09:55 +0200

    updated graphana docker run command to mount volume for ini

--- docker_run.sh
+++ docker_run.sh
@@ -6,17 +6,17 @@ echo $?
 
 docker run --name postgres --net takeaway_assignment -e POSTGRES_PASSWORD=airflow -d postgres
 echo $?
+sleep 10
+
+docker run --restart always --name smtp -d bytemark/smtp
+exho $?
 
 sleep 10
-#docker run -d --name graphite -p 80:80 -p 2003:2003 -p 2003:2003/udp -p 2004:2004 -p 7002:7002  --net takeaway_assignment  mrlesmithjr/graphite\n
-#echo $?
-
-sleep 10
-docker run -i -d --name grafana --net takeaway_assignment -p 3000:3000 grafana/grafana
+docker run -i -d --name grafana --net takeaway_assignment -v /Users/shubhigupta/github/takeaway/graphana/conf/defaults.ini:/usr/share/graphana/conf/defaults.ini -p 3000:3000 grafana/grafana
 echo $?
 
 sleep 10
-docker run -d -p 8080:8080 --net takeaway_assignment --link postgres:postgres -v /Users/shubhigupta/github/takeaway_assignment/airflow/dags/:/usr/local/airflow/dags airflow webserver
+docker run -d -p 8080:8080 --net takeaway_assignment --link postgres:postgres -v /Users/shubhigupta/github/takeaway/airflow/dags/:/usr/local/airflow/dags airflow webserver
 echo $?
 
 sleep 10
